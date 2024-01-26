81b6392d63d089550c9f520454e33728f3579eda
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-01-26 13:43:18 -0500

    Added route handler code BEFORE including MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { todoSchema, todoUpdateSchema } = require("./types");
 
 const PORT = 3000;
 const app = express();
@@ -10,13 +11,44 @@ app.use(express.json());
 // Defining the routes
 
 // Route to CREATE a new Todo
-app.post("/todo", (req, res) => {});
+app.post("/todo", (req, res) => {
+  // const todoTitle = req.body.title;
+  // const todoDescription = req.body.description;
+  // const todoStatus = req.body.completed;
+  const newTodo = todoSchema.safeParse(req.body);
+
+  if (newTodo.success) {
+    res.status(200).json({
+      message: "New todo has been created",
+    });
+    // put in mongoDB
+  } else {
+    res.status(411).json({
+      message: "Some wrong type of data has been passed",
+    });
+  }
+});
 
 // Route to READ all Todo's
-app.get("/todos", (req, res) => {});
+app.get("/todos", (req, res) => {
+  //get todos from mongoDB
+});
 
 // Route to UPDATE the completion of todo's
-app.put("/completed", (req, res) => {});
+app.put("/completed", (req, res) => {
+  const id = todoUpdateSchema.safeParse(req.body);
+
+  if (id.success) {
+    res.status(200).json({
+      message: "Marked as completed",
+    });
+    // Update mongo DB
+  } else {
+    res.status(411).json({
+      message: "Wrong ID",
+    });
+  }
+});
 
 // Server is listening on the PORT
 app.listen(PORT);
