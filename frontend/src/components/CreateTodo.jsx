78cf77774eac2c426c4191ccdf65715450740970
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-01-27 15:22:28 -0500

    added mark as complete functionality

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,34 +1,3 @@
-// import { useState } from 'react'
-
-// export function CreateTodo() {
-//     const [title, setTitle] = useState("");
-//     const [description, setDescription] = useState("");
-
-//     return <div>
-//         <input type="text" placeholder="Title" onChange={(e) => {
-//             setTitle(e.target.value);
-//         }}></input>
-//         <input type="text" placeholder="Description" onChange={(e) => {
-//             setDescription(e.target.value);
-//         }}></input>
-//         <button onClick={async () => {
-//             await fetch("http://localhost:3000/todo", {
-//                 method: "POST",
-//                 body: {
-//                     title: title,
-//                     description: description,
-//                     completed: false
-//                 },
-//                 headers: {
-//                     "Content-Type": "application/json"
-//                 }
-//             })
-//             const data = await res.json()
-//             alert(data)
-//             alert("Todo added!")
-//         }}>Add Todo</button>
-//     </div>
-// }
 import { useState } from 'react';
 
 export function CreateTodo() {
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-export function Todos({todos}) {
-    return <div>
-        {todos.map((element) => {
-            return <div>
-                <h1>{element.title}</h1>
-                <p>{element.description}</p>
-                <button>{todos.completed == true ? "Completed" : "Mark as Complete"}</button>
-            </div>
-        })}
+export function Todos({ todos }) {
+  const updateTodos = async (id) => {
+    await fetch("http://localhost:3000/completed", {
+      method: "PUT",
+      body: JSON.stringify({
+        id: id
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(async (data) => {
+      const res = await data.json();
+      alert(res);
+    });
+  };
+
+  return (
+    <div>
+      {todos.map((element) => (
+        <div key={element._id}>
+          <h1>{element.title}</h1>
+          <p>{element.description}</p>
+          <button onClick={() => updateTodos(element._id)}>
+            {element.completed ? "Completed" : "Mark as Complete"}
+          </button>
+        </div>
+      ))}
     </div>
+  );
 }
