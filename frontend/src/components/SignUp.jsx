69d9e4562411b2e169b90c4025b9d3e873d8991e
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-02-04 23:05:12 -0500

    Added some more CSS to Sign Up page

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -10,7 +10,7 @@ export default function SignUp() {
                         <p className='text-xs pb-5 pt-4 pr-1'>Already have an account? <a className='text-xs underline decoration-{sky-500} text-sky-500 pl-1' href="/signin">Sign in</a></p>
                     </div>
                     <div className="text-4xl font-bold text-gray-900 py-8 px-5">
-                        <p>Sign Up</p>
+                        <p>Sign Up 📝</p>
                     </div>
                     <div className="grid grid-cols-2 gap-1">
                         <Field label="Name" givenType="text"/>
@@ -31,7 +31,7 @@ export default function SignUp() {
 function Field(props) {
     return(
         <div className="py-2 px-5">
-            <p className='text-xs text-gray-500 pb-2'>{props.label}</p>
+            <p className='text-xs pb-2'>{props.label}</p>
             <input className='px-2 py-2 border-solid border-2 text-xs border-gray-500/50 rounded-lg w-full' type={props.givenType}></input>
         </div>
     )
