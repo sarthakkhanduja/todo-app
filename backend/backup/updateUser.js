033158cc36671c5f94957c422b761b286f4c0fb7
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-02-09 01:56:22 -0500

    updated Users table for new Schema

---
+++ backend/backup/updateUser.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://sarthakkhanduja:Sarthak%40123@test-cluster-sarthak.fxb1y3m.mongodb.net/todoApp"
+);
+
+const User = mongoose.model("User", {
+  name: String,
+  email: String,
+  password: String,
+});
+
+// Function to update Todo documents
+async function updateUsers() {
+  try {
+    await User.updateMany({}, [
+      { $set: { project: [] } }, // Add status with default value and set project to null
+    ]);
+
+    console.log("Users updated");
+  } catch (error) {
+    console.error("Error updating Users:", error);
+  }
+}
+
+// Update Todos, Users, and Projects
+async function updateData() {
+  try {
+    await updateUsers();
+  } finally {
+    // Close the MongoDB connection
+    mongoose.connection.close();
+  }
+}
+
+// Run the update process
+updateData();
+console.log("Done!");
