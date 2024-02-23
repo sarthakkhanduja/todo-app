d29f2dee1b64f08fcd6bc1e9bc615bdeaba70065
Sarthak Khanduja <sarthakkhanduja98@gmail.com>
2024-02-23 12:25:03 -0500

    Improved the touch length for swiping to see projectsH

--- frontend/src/components/UserHome.jsx
+++ frontend/src/components/UserHome.jsx
@@ -116,7 +116,7 @@ export function UserHome() {
     const [touchEnd, setTouchEnd] = useState(null)
 
     // the required distance between touchStart and touchEnd to be detected as a swipe
-    const minSwipeDistance = 40
+    const minSwipeDistance = 60
 
     const onTouchStart = (e) => {
     setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
